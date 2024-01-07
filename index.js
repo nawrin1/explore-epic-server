4f7b6e142e7b0713a01b55fb4200859fc2e764bb
Jannatul Ferdous Nawrin <jannatulnawrin@gmail.com>
2024-01-08 01:28:36 +0600

    token generated and verified

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors=require('cors')
 const port = process.env.PORT || 5000;
 
 require('dotenv').config()
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 // const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
@@ -55,8 +55,61 @@ const packageCollection = client.db("exploreDb").collection("package");
 const bookCollection = client.db("exploreDb").collection("booking");
 
 
+app.post('/jwt', async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' });
+  res.send({ token });
+})
+const verifyToken = (req, res, next) => {
+  // console.log('inside verify', req.headers.authorization);
+  if (!req.headers.authorization) {
+    console.log('token prob?')
+
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = req.headers.authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log('token prob')
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
+const verifyAdmin = async (req, res, next) => {
+  const email = req.decoded.email;
+  console.log(email,"is it admin? verify admin")
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  const isAdmin = user?.role === 'admin';
+  if (!isAdmin) {
+    console.log('.......')
+    return res.status(403).send({ message: 'forbidden access' });
+  }
+  next();
+}
+app.get('/users/admin/:email', verifyToken,async (req, res) => {
+  const email = req.params.email;
+
+  if (email !== req.decoded.email) {
+    console.log('nottt veridyyy')
+    return res.status(403).send({ message: 'forbidden access' })
+  }
+
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  let admin = false;
+  if (user) {
+    admin = user?.role === 'admin';
+    console.log('admin found')
+  }
+  res.send({ admin });
+})
 app.post('/users',async(req,res)=>{
     const user=req.body
+    console.log(user,"from server user")
     const query={email:user.email}
     const userExist=await userCollection.findOne(query)
     if (userExist){
@@ -80,14 +133,14 @@ app.get('/users',async(req,res)=>{
 })
 app.get('/package',async(req,res)=>{
   const id=req.query.id
-  console.log(id,"id from server")
+  // console.log(id,"id from server")
   let filter={}
 if (id){
   filter = { _id: new ObjectId(id) }
 
 }
   const packages=await packageCollection.find(filter).toArray()
-  console.log(packages,"pacages frm server")
+  // console.log(packages,"pacages frm server")
   res.send(packages)
 })
 app.get('/', async(req, res) => {
